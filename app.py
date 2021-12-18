a171d0034ab9fb441b953a9e9347495cafe0d288
NicholasK <nicholask1@outlook.com>
2021-12-18 08:04:26 -0500

    Finished templating the rest of the routes

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from surveys import *
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "Nicj"
-
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
@@ -30,6 +28,8 @@ def get_home_page():
 
     return render_template("home_page.html", survey=satisfaction_survey, r=RESPONSES)
 
+# Routes for the first question
+
 
 @app.route("/questions/0")
 def get_question0():
@@ -43,10 +43,12 @@ def save_first_question():
 
     return redirect("/question/1")
 
+# Routes for the second question
+
 
 @app.route("/questions/1")
 def get_question1():
-    return render_template("question1.html", question=questions[2])
+    return render_template("question1.html", question=questions[1])
 
 
 @app.route("/question/1", methods=["POST", "GET"])
@@ -54,4 +56,33 @@ def save_second_question():
     answer = request.form["answer"]
     RESPONSES.append(answer)
 
-    return redirect("/question/1")
+    return redirect("/question/2")
+
+# Routes for the third question
+
+
+@app.route("/questions/2")
+def get_question1():
+    return render_template("question2.html", question=questions[2])
+
+
+@app.route("/question/2", methods=["POST", "GET"])
+def save_second_question():
+    answer = request.form["answer"]
+    RESPONSES.append(answer)
+
+    return redirect("/question/3")
+
+
+# Routes for the fourth question
+@app.route("/questions/3")
+def get_question1():
+    return render_template("question3.html", question=questions[3])
+
+
+@app.route("/question/3", methods=["POST", "GET"])
+def save_second_question():
+    answer = request.form["answer"]
+    RESPONSES.append(answer)
+
+    return redirect("/thank-you")
