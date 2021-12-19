cd79e35f63304d0a2b29a12a627b6dd55c75e7c6
NicholasK <nicholask1@outlook.com>
2021-12-19 10:12:44 -0500

    Optumized pages, and added session data

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
+from flask.helpers import flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Nicj"
+app.config['SECRET_KEY'] = "Nick"
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-RESPONSES = []
+RESPONSES = "Answers"
+
 satisfaction_survey = Survey(
     "Customer Satisfaction Survey",
     "Please fill out a survey about your experience with us.",
@@ -20,69 +22,43 @@ satisfaction_survey = Survey(
                  ["Less than $10,000", "$10,000 or more"]),
         Question("Are you likely to shop here again?"),
     ])
+
 questions = satisfaction_survey.questions
 
 
 @app.route("/")
 def get_home_page():
 
+    session[RESPONSES] = []
+
     return render_template("home_page.html", survey=satisfaction_survey, r=RESPONSES)
 
 
-# Routes for the first question
-@app.route("/questions/0")
-def get_question0():
-    return render_template("question0.html", question=questions[0])
+@app.route("/questions/<int:id>")
+def get_question(id):
+    answers = session.get(RESPONSES)
+
+    if id != len(answers):
+        flash("Invalid question ID")
+        return redirect(f"/questions/{len(answers)}")
+
+    else:
+        question = questions[id]
+        return render_template("question.html", question=question)
 
 
-@app.route("/answer/0", methods=["POST", "GET"])
-def save_first_question():
+@app.route("/answer", methods=["POST", "GET"])
+def save_question():
+
     answer = request.form["answer"]
-    RESPONSES.append(answer)
+    answers = session[RESPONSES]
+    answers.append(answer)
+    session[RESPONSES] = answers
 
-    return redirect("/questions/1")
-
-
-# Routes for the second question
-@app.route("/questions/1")
-def get_question1():
-    return render_template("question1.html", question=questions[1])
-
-
-@app.route("/answer/1", methods=["POST", "GET"])
-def save_second_question():
-    answer = request.form["answer"]
-    RESPONSES.append(answer)
-
-    return redirect("/questions/2")
-
-
-# Routes for the third question
-@app.route("/questions/2")
-def get_question2():
-    return render_template("question2.html", question=questions[2])
-
-
-@app.route("/answer/2", methods=["POST", "GET"])
-def save_third_question():
-    answer = request.form["answer"]
-    RESPONSES.append(answer)
-
-    return redirect("/questions/3")
-
-
-# Routes for the fourth question
-@app.route("/questions/3")
-def get_question3():
-    return render_template("question3.html", question=questions[3])
-
-
-@app.route("/answer/3", methods=["POST", "GET"])
-def save_fourth_question():
-    answer = request.form["answer"]
-    RESPONSES.append(answer)
-
-    return redirect("/thank-you")
+    if len(answers) == len(questions):
+        return redirect("/thank-you")
+    else:
+        return redirect(f"/questions/{len(answers)}")
 
 
 @app.route("/thank-you")
